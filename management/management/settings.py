dfb85425b4f320f01a75acb9f7626f663b904d45
TuDeThuong <nguyenanhtu1604@gmail.com>
2023-10-23 16:18:36 +0200

    feat: Add user model

--- management/management/settings.py
+++ management/management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+AUTH_USER_MODEL = 'core.User'
